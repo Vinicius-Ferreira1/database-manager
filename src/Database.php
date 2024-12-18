a1283dd030c58e8b2bf55d1011a0be10c2f523ed
Vinicius Victor <vinicius.ferreira6@estudante.ifms.edu.br>
2024-12-18 14:35:41 -0400

    first commit

---
+++ src/Database.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Lades\DatabaseManager;
+
+use \PDO;
+use \PDOException;
+
+class Database{
+
+    private static $host;
+    private static $name;
+    private static $user;
+    private static $pass;
+    private static $port;
+    private static $table;
+    private static $connection;
+    
+    public function __construct($table){
+        $this->$table = $table;
+        $this->setConection();
+    }
+
+    public static function config($host, $name, $user, $pass = '', $port = 3306){
+        self::$host = $host;
+        self::$name = $name;
+        self::$user = $user;
+        self::$pass = $pass;
+        self::$port = $port;
+    }
+
+
+    private function setConection(){
+        try{
+            $this->connection = new PDO("mysql:host=".self::$host.";dbname=".self::$name.";port=".self::$port, self::$user, self::$pass);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            die("Erro: ".$e->getMessage());
+        }
+    }
+
+    public function execute($query, $params = []){
+        try{
+            $stmt = $this->connection->prepare($query);
+            $stmt->execute($params);
+            return $stmt;
+        }catch(PDOException $e){
+            die("Erro: ".$e->getMessage());
+        }
+    }
+
+    public function insert($values){
+
+        $fields = array_keys($values);
+        $binds = array_pad([], count($fields), '?');
+
+        $sql = "INSERT INTO ".$this->table." (".implode(',', $fields).") VALUES (".implode(',', $binds).")";
+
+        $this->execute($sql, array_values($values));
+
+        return $this->connection->lastInsertId();
+    }
+
+    public function select($join = null, $where = null, $order = null, $limit = null, $fields = '*'){
+        $where = strlen($where) ? "WHERE ".$where : '';
+        $order = strlen($order) ? "ORDER BY ".$order : '';
+        $limit = strlen($limit) ? "LIMIT ".$limit : '';
+
+        $sql = "SELECT ".$fields." FROM ".$this->table." ". $join ." ".$where." ".$order." ".$limit;
+
+        return $this->execute($sql);
+    }
+
+    public function update($where, $values){
+        $fields = array_keys($values);
+
+        $sql = "UPDATE ".$this->table." SET ".implode('=?,', $fields)."=? WHERE ".$where;
+
+        $this->execute($sql, array_values($values));
+
+        return true;
+    }
+
+    public function delete($where){
+        $sql = "DELETE FROM ".$this->table." WHERE ".$where;
+
+        $this->execute($sql);
+
+        return true;
+    }
+
+}
